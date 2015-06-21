23e8ceba542d332dc983631a95f95ad170a701c1
yannn12 <yannn12@gmail.com>
2015-06-21 18:09:41 +0300

    add Proxy test

---
+++ jar/proxy1.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+
+if "%JAVA_HOME%" == "" goto noJavaHome
+if not exist "%JAVA_HOME%\bin\java.exe" goto END
+
+"%JAVA_HOME%\bin\java.exe" -jar proxy.jar -com COM1 -inip 192.168.43.187 -inport 9999 -outip 192.168.43.187 -outport 8888
+goto END
+
+
+:noJavaHome
+@echo please set up JAVA_HOME environment variable - java should be version 8 and 32bit 
+goto END
+
+:END
+
